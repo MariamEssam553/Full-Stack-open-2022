4ddd8c19c28006967e548fae6d51acb5f5e20637
MariamEssam553 <mariamessammohamed98@gmail.com>
2022-07-03 03:47:19 +0200

    part1 ex1.2 solved

---
+++ part1/src/App2.js
@@ -0,0 +1,64 @@
+const Header = (props) => {
+    return(
+      <h1>{props.name}</h1>
+    )
+  }
+  
+//   const Content = (props) =>{
+    // return(
+        // <div>
+     //       <Part nPart={props.nPart} nEx={props.nEx} /> 
+      //   </div> 
+    // )
+//   }
+
+const Content = () =>{
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+    return(
+        <>
+            <Part nPart={part1} nEx={exercises1} />
+            <Part nPart={part2} nEx={exercises2} />
+            <Part nPart={part3} nEx={exercises3} />
+        </ >
+    )
+  }
+
+  const Part = (props) => {
+    return(
+        <p>{props.nPart} {props.nEx}</p>
+    )
+  }
+  
+  const Total = (props) =>{
+    return(
+      <p> Number of exercises {props.num}  </p>
+    )
+  }
+  
+  const App2 = () => {
+    const course = 'Half Stack application development'
+   // const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+   // const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    //const part3 = 'State of a component'
+    const exercises3 = 14
+  
+    return (
+      <>
+        <Header name={course} />
+        <Content />
+         {/* <Content nPart={part1} nEx={exercises1}  /> */}
+         {/* <Content nPart={part2} nEx={exercises2} /> */}
+         {/* <Content nPart={part3} nEx={exercises3} /> */}
+        <Total num={exercises1 + exercises2 + exercises3} />
+      </ >
+    )
+  }
+  
+  export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+//import App from './App'
+import App2 from './App2'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+//ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App2 />)
